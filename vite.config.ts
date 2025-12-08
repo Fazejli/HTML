c3303edeab95dd0406105c59b2a0f12de681f3bf
fadzejli <fadzejli@student.42.fr>
2025-12-08 11:44:16 +0100

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_ACTIONS ? "/" : "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
+  base: '/vti/', // Nom de votre repo GitHub
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
